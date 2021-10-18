c1f62a3f80338db76cc4279f354435c587bfdb2c
Thalyson Luiz <thalysonluiz@gmail.com>
2021-10-18 13:00:03 -0300

    Upload de arquivos

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import homeRoutes from './src/routes/homeRoutes';
 import usuarioRoutes from './src/routes/usuarioRoutes';
 import tokenRoutes from './src/routes/tokenRoutes';
 import alunoRoutes from './src/routes/alunoRoutes';
+import fotoRoutes from './src/routes/fotoRoutes';
 
 import './src/database';
 
@@ -27,6 +28,7 @@ class App {
     this.app.use('/usuarios/', usuarioRoutes);
     this.app.use('/tokens/', tokenRoutes);
     this.app.use('/alunos/', alunoRoutes);
+    this.app.use('/fotos/', fotoRoutes);
   }
 }
 
---
+++ src/controllers/FotoController.js
@@ -0,0 +1,21 @@
+import multer from 'multer';
+
+import multerConfig from '../config/multer';
+
+const upload = multer(multerConfig).single('foto');
+
+class FotoController {
+  async store(req, res) {
+    return upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({
+          errors: [err.code],
+        });
+      }
+
+      res.json(req.file);
+    });
+  }
+}
+
+export default new FotoController();
